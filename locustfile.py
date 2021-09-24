8f4ba4070cb7ac0f289f4671c526ffd8e7c44a54
Michael W <mwarr3n@gmail.com>
2021-09-24 15:53:08 -0500

    locust tests

--- locustfile.py
+++ locustfile.py
@@ -6,8 +6,12 @@ class WebsiteTestUser(HttpUser):
 
     @task(1)
     def test1(self):
-        self.client.get("http://localhost:8000")
+        self.client.get("http://localhost:5000")
 
     @task(2)
     def test2(self):
-        self.client.post("http://localhost:8000/predict")
+        payload = {"CHAS":{"0":0},"RM":{"0":6.575},"TAX":{"0":296.0},"PTRATIO":{"0":15.3},"B":{"0":396.9},"LSTAT":{"0":4.98}}
+
+        headers = {'content-type': 'application/json'}
+
+        self.client.post("http://localhost:5000/predict", json=payload, headers=headers)
